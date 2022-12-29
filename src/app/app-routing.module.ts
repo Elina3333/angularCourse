98502c274dad4b8d4a3b91016e294117b097db54
viktar33 <viktar.varabei@mail.ru>
2022-12-29 13:29:44 +0300

    Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,20 @@
-import {NgModule} from "@angular/core";
-import {RouterModule, Routes} from "@angular/router";
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {PostComponent} from "./post/post.component";
-import {PostsComponent} from "./posts/posts.component";
-import {AboutExtraComponent} from "./about-extra/about-extra.component";
-import {ErrorPageComponent} from "./error-page/error-page.component";
-import {AuthGuard} from "./auth.guard";
-import {PostResolver} from "./post.resolver";
+import {NgModule} from '@angular/core'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
+import {HomePageComponent} from './home-page/home-page.component'
+import {AboutPageComponent} from './about-page/about-page.component'
+import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
 
-//http://localhost:4200/
+const routes : Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(a => a.AboutPageModule)}
+  // {path:'about',loadChildren:'./about-page/about-page.module#AboutPageModule'} OLD version
+]
 
-const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'about',canActivateChild:[AuthGuard],component:AboutComponent,children:[
-      {path : 'extra',component:AboutExtraComponent}
-    ]},
-  {path:'posts',component:PostsComponent,canActivate:[AuthGuard]},
-  {path:'posts/:id',component:PostComponent,resolve : {post:PostResolver}},
-  {path:'error',component:ErrorPageComponent},
-  {path:'**',redirectTo:'/error'}
-];
 
 @NgModule({
-imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
+  exports: [RouterModule]
 })
-
 export class AppRoutingModule {
 
 }
